4e3c65432af756ba3c05f23736afb5d0f0d7c32d
mparedes003 <onnabugeishadev@gmail.com>
2020-03-11 16:59:20 -0700

    Create users table

---
+++ data/migrations/20200309145511_users.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("users", tbl => {
+    tbl.increments("id");
+
+    tbl
+      .text("username", 128)
+      .unique()
+      .notNullable();
+
+    tbl.text("password").notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("users");
+};
