e2a74cdff9b329980f4eca3b3d80ba1e5a02ed05
mparedes003 <onnabugeishadev@gmail.com>
2020-03-25 14:12:40 -0700

    Create ingredients-router for updating and deleteing ingredients by id.

--- api/ingredients/ingredients-router.js
+++ api/ingredients/ingredients-router.js
@@ -25,4 +25,41 @@ router.post(`/`, (req, res) => {
     });
 });
 
+// UPDATE ingredient by ingredient id
+router.put(`/:id`, (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db.updateIngredient(id, changes)
+    .then(ingredient => {
+      if (ingredient) {
+        res.status(200).json(ingredient);
+      } else {
+        res.status(404).json({ message: 'Ingredient not found' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to update ingredient' });
+    });
+});
+
+// DELETE ingredient by ingredient id
+router.delete(`/:id`, (req, res) => {
+  const { id } = req.params;
+
+  db.deleteIngredient(id)
+    .then(ingredient => {
+      if (ingredient) {
+        res.status(200).json({ message: 'Ingredient deleted successfully.' });
+      } else {
+        res.status(404).json({
+          message: 'Ingredient not found. Nothing to delete.'
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to delete ingredient' });
+    });
+});
+
 module.exports = router;
