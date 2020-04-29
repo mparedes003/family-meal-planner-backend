bde418e7ade43811fb6a2ec0f2f7752c85210de4
mparedes003 <onnabugeishadev@gmail.com>
2020-04-29 13:36:49 -0700

    Add instructions when adding a new recipe.

--- data/migrations/20200310203338_instructions.js
+++ data/migrations/20200310203338_instructions.js
@@ -1,21 +1,21 @@
-exports.up = function(knex) {
-  return knex.schema.createTable("instructions", tbl => {
-    tbl.increments("id");
+exports.up = function (knex) {
+  return knex.schema.createTable('instructions', (tbl) => {
+    tbl.increments('id');
 
     tbl
-      .integer("recipe_id")
+      .integer('recipe_id')
       .notNullable()
-      .references("id")
-      .inTable("recipes")
-      .onUpdate("CASCADE")
-      .onDelete("CASCADE");
+      .references('id')
+      .inTable('recipes')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
 
-    tbl.integer("step_number");
+    tbl.integer('step_number');
 
-    tbl.text("description");
+    tbl.text('description');
   });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("instructions");
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('instructions');
 };
