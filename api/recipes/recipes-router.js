0e4b08861ac963af6d36f64783d5df544ed71355
mparedes003 <onnabugeishadev@gmail.com>
2020-09-19 10:39:51 -0700

    DELETE Works: Add delete recipe endpoint to recipes-router.

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -23,13 +23,11 @@ router.get('/:id', async (req, res) => {
       let [recipe, ingredients, instructions] = rec;
       recipe = recipe[0];
       if (recipe) {
-        res
-          .status(200)
-          .json({
-            ...recipe,
-            ingredients: ingredients,
-            instructions: instructions,
-          });
+        res.status(200).json({
+          ...recipe,
+          ingredients: ingredients,
+          instructions: instructions,
+        });
       } else {
         res.status(404).json({ message: 'No recipe found with this ID.' });
       }
@@ -46,26 +44,44 @@ router.post('/', restricted, (req, res) => {
   const ownerId = req.user.id;
 
   db.addRecipe(recipe, ownerId)
-    .then((id) => {
-      db.findRecipeById(id)
-        // console
-        //   .log('recId found:', id)
-        .then((rec) => {
-          console.log('rec:', rec);
-          let [recipe, ingredients, instructions] = rec;
-          //recipe = recipe[0];
-          res.json({
-            ...recipe,
-            ingredients: ingredients,
-            instructions: instructions,
-          });
-        })
-        .catch((err) => {
-          res.status(500).json({ error: `Could not find recipe: ${err}` });
-        });
+    // .then((id) => {
+    //   db.findRecipeById(id)
+    // console
+    //   .log('recId found:', id)
+    .then((rec) => {
+      console.log('rec:', rec);
+      let [recipe, ingredients, instructions] = rec;
+      //recipe = recipe[0];
+      res.status(200).json({
+        ...recipe,
+        ingredients: ingredients,
+        instructions: instructions,
+      });
     })
     .catch((err) => {
-      res.status(500).json({ error: `Could not post recipe: ${err}` });
+      res.status(500).json({ error: `Could not find recipe: ${err}` });
+    });
+});
+//     .catch((err) => {
+//       res.status(500).json({ error: `Could not post recipe: ${err}` });
+//     });
+// });
+
+// DELETE a recipe
+router.delete('/:id', async (req, res) => {
+  const id = req.params;
+
+  db.deleteRecipe(id)
+    .then((count) => {
+      if (!count || count < 1) {
+        res.status(404).json({ message: 'No recipe found to delete' });
+      } else {
+        res.status(200).json(count);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ err });
     });
 });
 
